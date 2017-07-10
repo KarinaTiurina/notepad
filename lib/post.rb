cb1d909e18cee983595999eeb1314806d4bb45a3
KarinaTiurina <rktiurina@gmail.com>
2017-07-10 21:39:39 +0300

    Notepad v. 1, working

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,21 @@
 class Post
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    post_types[type_index].new
+  end
+
   def initialize
     @created_at = Time.now
     @text = nil
   end
 
   def read_from_console
-    # todo
   end
 
   def to_strings
-    # todo
   end
 
   def save
@@ -17,9 +23,9 @@ class Post
 
     for item in to_strings do
       file.puts(item)
-
-      file.close
     end
+
+    file.close
   end
 
   def file_path
@@ -27,6 +33,6 @@ class Post
 
     file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
 
-    current_path + "/../" + file_name
+    current_path + "/../notes/" + file_name
   end
 end
